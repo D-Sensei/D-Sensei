2c12a2aeefb2d902a4543f95c4fed551d0636152
Durvesh Gambhe <74155862+D-Sensei@users.noreply.github.com>
2023-03-02 19:23:41 +0530

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,12 @@
+
+username = "women"
+for i in range(3):
+    request = input("what kind of joke you want... Listening [][][]")
+
+    if request == username:   
+        print("women☕.")
+        print("hahaha . . ." )
+        print("sips")
+    else:
+        print("none")
+    print('want another joke on women. . .i got many')
